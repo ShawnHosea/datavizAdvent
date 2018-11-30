9942f12c8e2f6fec2efcdd6bd256ee87d2632beb
CraftAtoms <creator@craftatoms.com>
2018-11-29 19:19:27 -0500

    Fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import Header from '../components/header'
 import './layout.css'
 
 
@@ -45,7 +44,7 @@ const Layout = ({ children }) => (
 
         <html lang="en" />
         </Helmet>
-        <Header/>
+        
           {children}
       </div>
     )}
