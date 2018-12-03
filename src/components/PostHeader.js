c9b03efb78b6e305fbd54346d557f69af0d3e4b6
Swizec Teller <swizec@swizec.com>
2018-12-03 00:25:12 -0800

    Roughly put together each post

--- src/components/PostHeader.js
+++ src/components/PostHeader.js
@@ -2,20 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const PostHeader = props => {
-  return (
-    <pre
-    >
-      {props.postdate} /
-      <Link
-        
-        to={'/'}
-        aria-label="home"
-      >
-        {' '}
-        Santhosh Soundararajan
-      </Link>
-    </pre>
-  )
+  return <pre>{props.postdate} / Swizec Teller</pre>
 }
 
 export default PostHeader
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,25 +4,56 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import PostHeader from '../components/PostHeader'
 import MDXRenderer from 'gatsby-mdx/mdx-renderer'
+import ReactPlayer from 'react-player'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+`
+const Codesandbox = styled.iframe`
+  width: 100%;
+  height: 500px;
+`
+
+const Lead = styled.p`
+  font-size: 1.3em;
+`
 
 class BlogPostTemplate extends React.Component {
   render() {
     const { children, data, location, ...props } = this.props,
-      { mdx } = data
+      { mdx } = data,
+      { frontmatter } = mdx
 
     return (
       <Layout location={location}>
-        <Helmet
-          title={`${mdx.frontmatter.title} | ${data.site.siteMetadata.title}`}
-        />
-        <PostHeader postdate={mdx.frontmatter.date} />
-        <h1>{mdx.frontmatter.title}</h1>
+        <Wrapper>
+          <Helmet
+            title={`${frontmatter.title} | ${data.site.siteMetadata.title}`}
+          />
+          <PostHeader postdate={frontmatter.date} />
+          <h1>{frontmatter.title}</h1>
 
-        <div>
-          <MDXRenderer scope={this.props.__mdxScope}>
-            {mdx.code.body}
-          </MDXRenderer>
-        </div>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${frontmatter.videoId}`}
+          />
+
+          <Lead>{frontmatter.intro}</Lead>
+
+          <h2>Try it out 👇</h2>
+          <Codesandbox
+            src={`https://codesandbox.io/embed/${frontmatter.codesandboxId}`}
+            class="embedded-codesandbox"
+            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+          />
+
+          <div>
+            <MDXRenderer scope={this.props.__mdxScope}>
+              {mdx.code.body}
+            </MDXRenderer>
+          </div>
+        </Wrapper>
       </Layout>
     )
   }
@@ -47,6 +78,7 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         videoId
         codesandboxId
+        intro
       }
     }
   }
